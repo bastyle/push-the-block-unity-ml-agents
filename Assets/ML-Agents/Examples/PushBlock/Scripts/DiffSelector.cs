06578958a9533032d7bfb04535ec0edff086db70
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2024-04-09 12:28:04 -0400

    added scene selector

--- Assets/ML-Agents/Examples/PushBlock/Scripts/DiffSelector.cs
+++ Assets/ML-Agents/Examples/PushBlock/Scripts/DiffSelector.cs
@@ -6,10 +6,15 @@ using UnityEngine;
 
 public class DiffSelector : MonoBehaviour
 {
+
+    BehaviorParameters m_BehaviorParameters;
+
     void Awake()
     {
         string difficulty = PlayerPrefs.GetString("difficulty");
         Debug.Log("diffi::: " + difficulty);
+        m_BehaviorParameters = gameObject.GetComponent<BehaviorParameters>();
+        
     }
 
 }
--- Assets/ML-Agents/Examples/PushBlock/Scripts/DifficultySelector.cs
+++ Assets/ML-Agents/Examples/PushBlock/Scripts/DifficultySelector.cs
@@ -8,10 +8,26 @@ public class DifficultySelector : MonoBehaviour
 {
     public void OnButtonClick(string difficulty)
     {
-        Debug.Log("Button clicked! "+ difficulty);
+        Debug.Log("Button clicked! " + difficulty);
         PlayerPrefs.SetString("difficulty", difficulty);
         // create 3 different scenes
         SceneManager.LoadScene("GitGudFinalProjectPart2");
-        
+
+        switch (difficulty)
+        {
+            case "easy":
+                SceneManager.LoadScene("GitGudFinalProjectPart2Easy");
+                break;
+            case "medium":
+               SceneManager.LoadScene("GitGudFinalProjectPart2Medium");
+                break;
+            case "hard":
+               SceneManager.LoadScene("GitGudFinalProjectPart2EasyHard");
+                break;
+            default:
+                Debug.LogError("Invalid difficulty: " + difficulty);
+                break;
+        }
+
     }
 }
